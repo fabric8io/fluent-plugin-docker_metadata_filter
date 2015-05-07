c66a5d84f361ac10ee5d4b0518ac6bcb12087587
Jimmi Dyson <jimmidyson@gmail.com>
2015-05-07 14:17:06 +0100

    Ignore codeclimate.com requests in VCR

--- test/helper.rb
+++ test/helper.rb
@@ -37,6 +37,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes"
   config.hook_into :webmock # or :fakeweb
+  config.ignore_hosts 'codeclimate.com'
 end
 
 unless defined?(Test::Unit::AssertionFailedError)
