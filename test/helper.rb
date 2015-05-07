0f2833228fc7e587d9d954da82c11ba59a6d5d4a
Jimmi Dyson <jimmidyson@gmail.com>
2015-05-07 14:14:05 +0100

    Add test coverage

--- test/helper.rb
+++ test/helper.rb
@@ -16,29 +16,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# simplecov must be loaded before any of target code
-if ENV['SIMPLE_COV']
-  require 'simplecov'
-  if defined?(SimpleCov::SourceFile)
-    mod = SimpleCov::SourceFile
-    def mod.new(*args, &block)
-      m = allocate
-      m.instance_eval do
-        begin
-          initialize(*args, &block)
-        rescue Encoding::UndefinedConversionError
-          @src = "".force_encoding('UTF-8')
-        end
-      end
-      m
-    end
-  end
-  unless SimpleCov.running
-    SimpleCov.start do
-      add_filter '/test/'
-      add_filter '/gems/'
-    end
-  end
+#
+require "codeclimate-test-reporter"
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
 end
 
 require 'rr'
